8f72694bedca39c423bbe91ff7c2a6a914aadc4e
Gustavo Nobrega <gustavo.nobrega@hotmail.com>
2022-10-03 19:20:31 -0300

    Add Header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,24 @@
 import { AppProps } from "next/app"
 import { globalStyles } from "../styles/global"
 
+import Image from 'next/future/image'
+
+import logoImg from '../assets/logo.svg';
+import { Container, Header } from "../styles/pages/app";
+
+
 globalStyles();
 
 function App({ Component, pageProps }: AppProps) {
-  
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt=""/>
+      </Header>
+      
+      <Component {...pageProps} />
+    </Container>
+  )
 }
 
 export default App
