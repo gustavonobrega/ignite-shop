9c87374d68e378ef9098455a7c579eb1b2a748ec
Gustavo Nobrega <gustavo.nobrega@hotmail.com>
2022-10-04 15:21:39 -0300

    Implement Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,38 @@
+import Image from 'next/future/image'
+
+import { HomeContainer, Product,  } from '../styles/pages/home'
+
+import camiseta1 from '../assets/camisetas/Shirt.png'
+import camiseta2 from '../assets/camisetas/Shirt-1.png'
+import camiseta3 from '../assets/camisetas/Shirt-2.png'
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="" />
+      
+        <footer>
+          <strong>Camiseta 1</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt="" />
+      
+        <footer>
+          <strong>Camiseta 2</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta3} width={520} height={480} alt="" />
+      
+        <footer>
+          <strong>Camiseta 2</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
