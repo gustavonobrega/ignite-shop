4f5c21e13ea863e2c064574b31a5e1772180b5cc
Gustavo Nobrega <gustavo.nobrega@hotmail.com>
2022-10-04 19:04:41 -0300

    Update Home link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import { GetStaticProps } from 'next';
 import Image from 'next/future/image'
 import { useKeenSlider } from 'keen-slider/react'
+import Stripe from 'stripe';
+import Link from 'next/link';
 
 import { stripe } from '../lib/stripe';
 import { HomeContainer, Product,  } from '../styles/pages/home'
 
 import 'keen-slider/keen-slider.min.css'
-import Stripe from 'stripe';
 
 
 interface HomeProps {
@@ -31,14 +32,16 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider" >
       {products.map(product => {
         return (
-          <Product key={product.id} className="keen-slider__slide">
-            <Image src={product.imageUrl} width={520} height={480} alt="" />
-          
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+          <Link href={`/product/${product.id}`} key={product.id}>
+            <Product  className="keen-slider__slide">
+              <Image src={product.imageUrl} width={520} height={480} alt="" />
+            
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         )
       })}
     </HomeContainer>
