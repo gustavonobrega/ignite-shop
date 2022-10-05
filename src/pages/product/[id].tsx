395fd0b945fc5afe4c10d2af186e68157131ba25
Gustavo Nobrega <gustavo.nobrega@hotmail.com>
2022-10-05 16:07:21 -0300

    Implement Product page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,73 @@
-export default function Product() {
+import { GetStaticPaths, GetStaticProps } from 'next'
+import Image from 'next/future/image'
+import Stripe from 'stripe'
+import { stripe } from '../../lib/stripe'
+import { ImageContainer, ProductContainer, ProductDetails } from '../../styles/pages/product'
+
+interface ProductProps {
+  product: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: string;
+    description: string;
+  }
+}
+
+export default function Product({ product }: ProductProps) {
   return (
-    <div>
-      <h1>Product</h1>
-    </div>
+    <ProductContainer>
+      <ImageContainer>
+      <Image src={product.imageUrl} width={520} height={480} alt="" />
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
+
+        <p>{product.description}</p>
+
+        <button type='button'>
+          Comprar
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
+}
+
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [
+       { params: { id: 'prod_MYNoL6O07kukXc' } }
+    ],
+    fallback: 'blocking',
+  }
+}
+
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
+  const productId = params.id;
+  
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price'],
+  })
+  
+  const price = product.default_price as Stripe.Price
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL'
+        }).format(price.unit_amount / 100),
+        description: product.description,
+      }
+    },
+    revalidate: 60 * 60 * 1, // 1 hour
+  }
 }
