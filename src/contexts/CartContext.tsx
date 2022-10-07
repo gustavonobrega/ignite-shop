461f3b21272ea333a61e8f2497b017f52b9d52ee
Gustavo Nobrega <gustavo.nobrega@hotmail.com>
2022-10-07 15:28:21 -0300

    Implement CartContext

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -4,14 +4,35 @@ interface CartContextProviderProps {
   children: ReactNode;
 }
 
- interface CartContextData {}
+interface Product {
+  id: string;
+  name: string;
+  imageUrl: string;
+  price: string;
+  defaultPriceId: string;
+}
+
+ interface CartContextData {
+  cart: Product[];
+  addToCart: (product: Product) => void;
+  removeFromCart: (productId: string) => void;
+ }
 
 export const CartContext = createContext({} as CartContextData);
 
 export function CartContextProvider({ children }: CartContextProviderProps) { 
+  const [cart, setCart] = useState<Product[]>([]);
+
+  function addToCart(product: Product) {
+    setCart(state => [...state, product])
+  }
+
+  function removeFromCart(productId: string) {
+    setCart(state => state.filter(product => product.id !== productId));
+  }
 
   return (
-    <CartContext.Provider value={{  }} >
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart }} >
       {children}
     </CartContext.Provider>
    )
