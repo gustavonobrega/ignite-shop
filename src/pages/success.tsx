8585a27ef21d03a364358f98b9bf2f956934b428
Gustavo Nobrega <gustavo.nobrega@hotmail.com>
2022-10-11 16:23:08 -0300

    Update Success to can show all cart items

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -8,13 +8,11 @@ import { ImageContainer, SuccessContainer } from "../styles/pages/success";
 
 interface SuccessProps {
   customerName: string;
-  product: {
-    name: string,
-    imageUrl: string;
-  }
+  totalItems: number,
+  imagesUrl: string[],
 }
 
-export default function Success({ customerName, product }: SuccessProps) {
+export default function Success({ customerName, totalItems, imagesUrl }: SuccessProps) {
   return (
     <>
       <Head>
@@ -25,14 +23,20 @@ export default function Success({ customerName, product }: SuccessProps) {
 
 
       <SuccessContainer>
+        <ul>
+          {imagesUrl.map(image => (
+            <li key={image}>
+              <ImageContainer>
+                <Image  src={image} alt="" width={120} height={110}/>
+              </ImageContainer>
+            </li>
+          ))}
+        </ul>
+        
         <h1>Compra efetuada!</h1>
 
-        <ImageContainer>
-          <Image src={product.imageUrl} alt="" width={120} height={110}/>
-        </ImageContainer>
-        
         <p>
-          Uhuul, <strong>{customerName}</strong>, sua <strong>{product.name}</strong> já está a caminho de sua casa.
+          Uhuul, <strong>{customerName}</strong>, sua compra de <strong>{totalItems}</strong> camisetas já está a caminho de sua casa.
         </p>
       
         <Link href="/">
@@ -60,16 +64,16 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   });
 
   const customerName = session.customer_details.name;
-  const product = session.line_items.data[0].price.product as Stripe.Product
-
+  const products = session.line_items.data.map(item => item.price.product as Stripe.Product)
+  const images = products
+    .map(product => product.images[0])
+    
   return {
     props: {
       customerName,
-      product: {
-        name: product.name,
-        imageUrl: product.images[0]
-      },
-    }
+      totalItems: products.length,
+      imagesUrl: images,
+    },
   }
 }
 
--- src/styles/pages/success.ts
+++ src/styles/pages/success.ts
@@ -11,6 +11,7 @@ export const SuccessContainer = styled('main', {
   h1: {
     fontSize: '$2xl',
     color: '$gray100',
+    marginTop: '3rem'
   },
 
   p: {
@@ -35,16 +36,25 @@ export const SuccessContainer = styled('main', {
       color: '$green300',
     },
   },
+
+  ul: {
+    display: 'flex',
+    listStyle: 'none',
+
+    li: {
+      marginLeft: '-1.5rem'
+    }
+  }
 })
 
 export const ImageContainer = styled('div', {
   width: '100%',
+  borderRadius: '50%',
+  boxShadow: '0px 0px 60px rgba(0, 0, 0, 0.8)',
   maxWidth: 130,
   height: 145,
   background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
-  borderRadius: 8,
   padding: '0.25rem',
-  marginTop: '4rem',
 
   display: 'flex',
   alignItems: 'center',
